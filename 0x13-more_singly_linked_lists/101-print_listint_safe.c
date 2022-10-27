6b1462234d96a52164a8b334fc9402f7db24b122
Tiaha <56724110+john526@users.noreply.github.com>
2022-10-27 04:06:31 +0000

    file

--- 0x13-more_singly_linked_lists/101-print_listint_safe.c
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -1,55 +1,31 @@
 #include "lists.h"
-#include <stdio.h>
-
 /**
- * print_listint_safe - function that prints a listint_t linked list.
- * @head: pointer to head of a list.
- *
- * Return: Length of list (INT)
+ * print_listint_safe - prints linked list
+ * @head: beginning of linked list
+ * Return: number of elements in linked list
  */
 size_t print_listint_safe(const listint_t *head)
 {
-	const listint_t *slow_p = head ,*fast_p = head;
-	size_t ele = 0;
-	int is_loop = 0;
+	size_t i = 0, j;
+	const listint_t *temp_h = head, *check_next;
 
-	while (slow_p && fast_p && fast_p->next)
+	while (temp_h)
 	{
-		if (!(fast_p->next->next))
-			break;
-		slow_p = slow_p->next;
-		fast_p = fast_p->next->next;
-		if (slow_p == fast_p)
+		printf("[%p] %d\n", (void *)temp_h, temp_h->n);
+		i++;
+		temp_h = temp_h->next;
+		check_next = head;
+		j = 0;
+		while (j < i)
 		{
-			slow_p = slow_p->next;
-			is_loop = 1;
-			break;
+			if (temp_h == check_next)
+			{
+				printf("-> [%p] %d\n", (void *)temp_h, temp_h->n);
+				return (i);
+			}
+			check_next = check_next->next;
+			j++;
 		}
 	}
-
-	if (!is_loop)
-	{
-		while (head)
-		{
-			ele++;
-			printf("[%p] %d\n", (void *)head, head->n);
-			head = head->next;
-		}
-		return (ele);
-	}
-
-	while (head)
-	{
-		ele++;
-		if (head == slow_p)
-		{
-			printf("[%p] %d\n", (void *)head, head->n);
-			printf("-> [%p] %d\n", (void *)head, head->next->n);
-			exit(98);
-		}
-
-		printf("[%p] %d\n", (void *)head, head->n);
-		head = head->next;
-	}
-	return (0);
+	return (i);
 }
