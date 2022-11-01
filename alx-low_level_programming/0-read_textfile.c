69dfd5d8f817003c3f02a2a070a1b08d70281d46
Tiaha <56724110+john526@users.noreply.github.com>
2022-11-01 03:30:34 +0000

    file

--- alx-low_level_programming/0-read_textfile.c
+++ alx-low_level_programming/0-read_textfile.c
@@ -1,45 +1,32 @@
 #include "main.h"
-#include <unistd.h>
-#include <sys/types.h>
-#include <sys/stat.h>
-#include <fcntl.h>
-#include <stdlib.h>
-#include <stdio.h>
-
 /**
- * read_textfile - reads a txt file and prints it to standard output .
- * @filename: name of the txt file to read .
- * @letters: number of letters it should read and print
- *
- * Return: actual number of letters it could read and print
- */
+  * read_textfile - reads a text file and prints it to the POSIX standard out
+  * @filename: name of the file to read
+  * @letters: number of characters to print
+  * Return: 0 on success
+  **/
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-	int fd;
-	ssize_t r, w;
-	char *buffer;
+	int txt_file, total, read_status;
+	char buffer[BUFSIZE];
 
 	if (filename == NULL)
 		return (0);
-	fd = open(filename, O_RDWR);
-	if (fd == -1)
+	txt_file = open(filename, O_RDONLY);
+	if (txt_file == -1)
 		return (0);
-	buffer = malloc(letters);
-	if (buffer == NULL)
+	total = 0;
+	read_status = 1;
+	while (letters > BUFSIZE && read_status != 0)
 	{
-		close(fd);
-		return (0);
+		read_status = read(txt_file, buffer, BUFSIZE);
+		write(STDOUT_FILENO, buffer, read_status);
+		total += read_status;
+		letters -= BUFSIZE;
 	}
-	r = read(fd, buffer, letters);
-	close(fd);
-	if (r == -1)
-	{
-		free(buffer);
-		return (0);
-	}
-	w = write(STDOUT_FILENO, buffer, r);
-	free(buffer);
-	if (r != w)
-		return (0);
-	return (w);
+	read_status = read(txt_file, buffer, letters);
+	write(STDOUT_FILENO, buffer, read_status);
+	total += read_status;
+	close(txt_file);
+	return (total);
 }
--- alx-low_level_programming/2-append_text_to_file.c
+++ alx-low_level_programming/2-append_text_to_file.c
@@ -1,48 +1,25 @@
 #include "main.h"
-#include <unistd.h>
-#include <sys/types.h>
-#include <sys/stat.h>
-#include <fcntl.h>
-
+#include <stdio.h>
 /**
- * _strlen - finds the length of a string
- * @s : pointer to the string
- *
- * Return: length of the string
- */
-int _strlen(char *s)
-{
-	int length = 0;
-
-	while (s[length])
-	{
-		length++;
-	}
-
-	return (length);
-}
-
-/**
- * append_text_to_file - appends a text at the end of a file.
- * @filename: name of the file .
- * @text_content: text content to be written
- *
- * Return: 1 on success and -1 on failure
- */
+  * append_text_to_file - appends text to a file
+  * @filename: file to append to, if NULL, return -1
+  * @text_content: content to append, if NULL, do not append
+  * Return: 1 on success, -1 on failure
+  */
 int append_text_to_file(const char *filename, char *text_content)
 {
-	int fd;
-	int wlen;
+	int append_file, len, wr_stat;
 
 	if (filename == NULL)
 		return (-1);
-	fd = open(filename, O_WRONLY | O_APPEND);
-	if (fd == -1)
+	append_file = open(filename, O_WRONLY | O_APPEND);
+	if (append_file == -1)
 		return (-1);
-	if (text_content != NULL)
-		wlen = write(fd, text_content, _strlen(text_content));
-	close(fd);
-	if (wlen == -1)
-		return (-1);
-	return (1);
+	if (text_content == NULL)
+		return (1);
+	for (len = 0; text_content[len]; len++)
+		;
+	wr_stat = write(append_file, text_content, len);
+	close(append_file);
+	return (wr_stat == -1 ? -1 : 1);
 }
